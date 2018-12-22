1f7300e54128ec063774ffdb2f4738a4b7d4669c
Lee Dora <dsa28s@naver.com>
2018-12-22 17:08:09 +0900

    Initial commit;

---
+++ app/src/main/kotlin/ios/android/installable/youthquare/MainActivity.kt
@@ -0,0 +1,11 @@
+package ios.android.installable.youthquare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
